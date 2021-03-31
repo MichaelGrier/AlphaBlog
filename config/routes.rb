e0ca711ba73c8c531f3841103ef083b0b027708c
Michael Grier <michael.c.grier@gmail.com>
2021-03-31 09:23:38 -0600

    add signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   # resources :articles, only: %i[show index new create edit update] # allow only these routes
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
